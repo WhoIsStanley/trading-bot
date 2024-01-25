71a7a7f2301dcb2e19a7c4aa6379e1bc6ef034bf
WhoIsStanley <stanleychan246@gmail.com>
2024-01-25 17:30:24 +0800

    vue bot 25/1

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -1,11 +1,10 @@
-import { SlashCommandBuilder } from "discord.js";
+const { SlashCommandBuilder } = require('discord.js');
 
-export const command = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('reply ping value');
-
-
-//fixing about ping value reaction
-export const action = async(ctx) => {
-    ctx.reply('Bot is ready to use!');
-}
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
--- src/commands/test/index.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-
-export const command = new SlashCommandBuilder()
-    .setName('test')
-    .setDescription('reply a testing message');
-
-export const action = async(ctx) => {
-    ctx.reply('Bot is ready to use!');
-}
